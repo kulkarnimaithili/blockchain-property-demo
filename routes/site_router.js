c2e92ea207d37d0305c7d7f6bba933b6b5324a58
Aaron Colaco <taipan93@gmail.com>
2017-02-14 14:48:35 +0530

    Populating dropdown lists

--- routes/site_router.js
+++ routes/site_router.js
@@ -12,16 +12,20 @@ var express = require('express');
 var router = express.Router();
 var setup = require('../setup.js');
 
+const config = require('../config/');
+const owners = config.getOwners();
+const surveys = config.getSurveys();
+
 router.route('/').get(function(req, res){
   res.render('home', {title: 'Home'});
 });
 
 router.route('/registration').get(function(req, res){
-  res.render('registration', {title: 'Register Property'});
+  res.render('registration', {title: 'Register Property', owners });
 });
 
 router.route('/sale').get(function(req, res){
-  res.render('sales', {title: 'Sell Property'});
+  res.render('sales', {title: 'Sell Property', owners, surveys });
 });
 
 module.exports = router;
