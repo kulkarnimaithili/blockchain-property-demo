42e91c573d87d0585dc24a28c8f14ad18c99646e
Vivek Kumar <vivek.kumar26@live.com>
2017-02-14 23:40:22 +0530

    - Fixed array.push not working while fetching block stats

--- utils/ws_part1.js
+++ utils/ws_part1.js
@@ -4,35 +4,33 @@ var ibc = {};
 var chaincode = {};
 var async = require('async');
 
-module.exports.setup = function(sdk, cc){
+module.exports.setup = function(sdk, cc) {
   ibc = sdk;
   chaincode = cc;
 };
 
 
-module.exports.myProcessMsg = function(data, callback){
-  if(data.type == 'create'){  // create a new marble
+module.exports.myProcessMsg = function(data, callback) {
+  if (data.type == 'create') { // create a new marble
     console.log('its a create!');
 
     const property = data.property;
-    
-    if(property.owner && property.aadhar && property.area && property.location && property.survey){
+
+    if (property.owner && property.aadhar && property.area && property.location && property.survey) {
       chaincode.invoke.initProperty([property.owner, property.aadhar, property.survey, property.location, property.area], (err, response) => {
         if (err) {
           callback(err, null);
         } else {
           callback(null, response);
         }
-      }); 
+      });
     }
-  }
-  else if(data.type == 'transfer'){
+  } else if (data.type == 'transfer') {
     console.log('transfering msg');
-    if(data.name && data.user){
+    if (data.name && data.user) {
       chaincode.invoke.set_owner([data.name, data.user]);
     }
-  }
-  else if(data.type == 'chainstats'){
+  } else if (data.type == 'chainstats') {
     console.log('chainstats msg');
     ibc.chain_stats(myCbStats);
   }
@@ -44,89 +42,57 @@ module.exports.myProcessMsg = function(data, callback){
     }
 
     let blockCount = chainStats.height - 1;
-    let blockChain = {};
+    let blockChain = [];
 
-    /*for (let i = blockCount; i > 0; i--) {  // skip genesis block
-      let block = {};
-
-      // take only last 10 blocks
-      if (blockChain.length > 9) {
-        break;
-      }
-
+    for (let i = blockCount; i > (blockCount - 10); i--) { // skip genesis block
+      // Start fetching block stats
       ibc.block_stats(i, (e, stats) => {
         if (e) {
           return callback(e, null);
         }
-        // block = stats.transactions[0];
         blockChain.push(stats);
-        console.log("\n\n\n\n----------------------------------------------------------------------------------------------------\n");
-        console.log(blockChain.length);
-        console.log("\n-------------------------------------------------------------------------------------------------------------\n\n\n\n");
+        if (blockChain.length == 10) {
+          return callback(null, blockChain);
+        }
       });
     }
 
-
-    console.log("\n\n\n\n ---------");
-    console.log(blockChain);
-    console.log("\n\n\n\n ---------");
-    // blockChain.reverse();
-    return callback(null, {"Size": blockChain.length});*/
-
-
-
-    // let count = 0;
-
-
-    // async.eachLimit(blockChain, 1, (blockCount, cb) => {           //iter through each one, and send it
-    //   if (count++ < 10) {
-    //     ibc.block_stats(blockCount, (e, stats) => {
-    //       if(e == null){
-    //         blockChain.push(stats);
-    //       }
-    //       cb(null);
-    //     });
-    //   }
-    // }, () => {
-    //   setTimeout(() => {
-    //     return callback(null, {"blockChain": blockChain.length });
-    //   }, 1000);
-    // });
-
-
   }
 
-  
+
   //call back for getting the blockchain stats, lets get the block stats now
-  function cb_chainstats(e, chain_stats){
-    if(chain_stats && chain_stats.height){
-      chain_stats.height = chain_stats.height - 1;                //its 1 higher than actual height
+  function cb_chainstats(e, chain_stats) {
+    if (chain_stats && chain_stats.height) {
+      chain_stats.height = chain_stats.height - 1; //its 1 higher than actual height
       var list = [];
-      for(var i = chain_stats.height; i >= 1; i--){               //create a list of heights we need
+      for (var i = chain_stats.height; i >= 1; i--) { //create a list of heights we need
         list.push(i);
-        if(list.length >= 8) break;
+        if (list.length >= 8) break;
       }
-      list.reverse();                               //flip it so order is correct in UI
-      async.eachLimit(list, 1, function(block_height, cb) {           //iter through each one, and send it
-        ibc.block_stats(block_height, function(e, stats){
-          if(e == null){
+      list.reverse(); //flip it so order is correct in UI
+      async.eachLimit(list, 1, function(block_height, cb) { //iter through each one, and send it
+        ibc.block_stats(block_height, function(e, stats) {
+          if (e == null) {
             stats.height = block_height;
-            sendMsg({msg: 'chainstats', e: e, chainstats: chain_stats, blockstats: stats});
+            sendMsg({
+              msg: 'chainstats',
+              e: e,
+              chainstats: chain_stats,
+              blockstats: stats
+            });
           }
           cb(null);
         });
-      }, function() {
-      });
+      }, function() {});
     }
   }
-  
+
   //send a message, socket might be closed...
-  function sendMsg(json){
-    if(ws){
-      try{
+  function sendMsg(json) {
+    if (ws) {
+      try {
         ws.send(JSON.stringify(json));
-      }
-      catch(e){
+      } catch (e) {
         console.log('[ws error] could not send msg', e);
       }
     }
