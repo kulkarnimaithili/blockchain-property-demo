1c0269b1272a4be24c9425ea57df786519ad998a
haxreo5 <rishaband15051995@gmail.com>
2017-02-16 12:28:43 +0530

    Fixed Display of Blockview

--- public/js/blockview.js
+++ public/js/blockview.js
@@ -1,30 +1,38 @@
+var blocks = document.getElementById("hidden-data");
+var obj = JSON.parse(blocks.innerHTML);
+console.log(obj[0].stateHash)
+var i = 0, howManyTimes = 10;
+var q = document.createElement("div");
+q.className="row";
+function loadCards() 
+{     
+  var w = document.createElement("div");
+  w.className="col s6 m2 l3";
+  q.appendChild(w);
+  var e = document.createElement("div");
+  e.className="card blue-grey darken-1";
+  w.appendChild(e);
+  var r = document.createElement("div");
+  r.className="card-content white-text";
+  e.appendChild(r);
+  var para = document.createElement("code");
+  para.className = "show_details";
+  var content = document.createTextNode("Block"+ (i+1));
+  r.appendChild(para);
+  para.appendChild(content);
+  document.getElementById("mypara").appendChild(q);
+  var hoverDetails = document.createElement("code");
+  r.appendChild(hoverDetails);
+  hoverDetails.id = i;
+  hoverDetails.className = "hidden-details";
+  var contentx = document.createTextNode(JSON.stringify(obj[i], null, "\t"));
+  console.log(JSON.stringify(obj[i], null, "\t"));
+  hoverDetails.appendChild(contentx);
 
-    var blocks = document.getElementById("hidden-data");
-    
-    var obj = JSON.parse(blocks.innerHTML);
+  i++;
+    if( i < howManyTimes ){
+        setTimeout( loadCards, 100 );
+    }
+}
 
-      var i = 0, howManyTimes = 10;
-      var q = document.createElement("div");
-      q.className="row";
-      function displayBlocks() 
-      {   
-         var w = document.createElement("div");
-         w.className="col s6 m2";
-         q.appendChild(w);
-         var e = document.createElement("div");
-         e.className="card blue-grey darken-1";
-         w.appendChild(e);
-         var r = document.createElement("div");
-         r.className="card-content white-text";
-         e.appendChild(r);
-         var para = document.createElement("p");
-         var content = document.createTextNode(obj[i].stateHash);
-         r.appendChild(para);
-         para.appendChild(content);
-         document.getElementById("mypara").appendChild(q);
-         
-         i++;
-          if( i < howManyTimes ){
-               setTimeout( displayBlocks, 500 );
-          }
-      }
+
---
+++ public/js/hoverCard.js
@@ -0,0 +1,135 @@
+*
+ *  Hover Card - v0.2.2
+ *  Made by Sahil Prajapati
+ */
+(function( $ ) {
+ 
+    $.fn.hoverCards = function(options) {
+ 
+        var settings = $.extend({}, $.fn.hoverCards.defaults, options );
+        var card;
+        var showCard = false;
+        var hCard = $('<div/>');
+        var request;
+        this.addClass('hover-activated');
+
+    	$('.hover-activated').stop(true, true).hover(function(){
+    		card = $(this);
+    		timeout = setTimeout(function(){
+            showCard = true;
+            if(showCard === true){
+		      	var dev_id = card.attr('data-dev-id');
+		    	if(card.find('.hoverParent').length == 0){
+		    		if (request != undefined){
+		    			request.abort();
+		    		}
+			 		getData(settings.url, dev_id);
+			 	}
+			 	else{
+			 		hCard = card.find('.hoverParent');
+			 		var page_height = $(window).scrollTop();
+		    		var elem_height = card.offset().top;
+			 		positionCard(page_height, elem_height);	
+		 		}
+		 	}
+		 },settings.delay);
+	 	},
+	    function(){
+	    	//else don't show and clearout timer
+            clearTimeout(timeout);
+            showCard = false;
+	    	card.find('.hoverParent').fadeOut(settings.fadeOut).find('.hoverCard').hide();
+	    });
+
+
+	    function getData(url, id){
+			
+			request = $.ajax({
+		        url: url+id,
+		        method: 'get',
+		        dataType: 'json',
+		        beforeSend: function(data){
+		        	hCard.addClass('hoverParent').css('left','-7px').fadeIn(settings.fadeIn);//.append('<div class="hoverCard in-block">Loading..</div>');
+		        	card.css('position','relative').append(hCard);
+		        },
+		        success: function(data){
+		        	var user = data;
+		          	//calling hover plugin
+		          	createCard(user);
+		        },
+		        error:function(){
+
+		        }
+		      });
+	    	
+	    }
+
+	    function createCard(user){
+	        var data = '<div class="hoverCard"><div class="arrow-up"></div><div class="userPic" style="background-color:'+settings.backgroundColor+'"><div class="picContainer"><img src="'+user.pic+'"></div>';
+	        data += '<div class="userDetail"><span class="name"><strong>'+user.name+'</strong></span>';
+	        data += '<p class="small role text-muted">'+user.role+'</p>';
+	        if (user.hasOwnProperty("about")){
+	        	if(user.about.length > 140 ){
+	        		data += '<p class="about small">'+user.about.substring(0,140)+'..</p></div></div>';
+	        	}
+	        	else{
+	        		data += '<p class="about small">'+user.about.substring(0,140)+'</p></div></div>';
+	        	}
+
+	    	}
+        	data += '<div class="userStats"><div><strong>'+user.solved+'</strong><p>Problems Solved</p></div>';
+        	data += '<div><strong>'+user.followers+'</strong><p>Followers</p></div>';
+        	data += '<div><strong>'+user.following+'</strong><p>Following</p></div></div><div class="arrow-down"></div></div>';
+        	attachCard(data);	        
+	    }
+
+	    function attachCard(data){
+	    	var page_height = $(window).scrollTop();
+	    	var elem_height = card.offset().top;
+	    	hCard.html(data);
+	    	positionCard(page_height, elem_height);
+	    	
+	    }
+
+	    function positionCard(page_height, elem_height){
+	    	var left = card.offset().left;
+	    	var page_width = $(window).width();
+	    	if(elem_height-page_height > 250){
+	    		//top-left
+	    		if(page_width - left < 335){
+		    		hCard.css({'top':'-20px','height': '40px','display':'block','left':'-209px'}).find('.hoverCard').css({'bottom':'30px','top':'none','display':'block'});
+		    		hCard.find('.arrow-down').show().css('left','240px');
+		    		hCard.find('.arrow-up').hide().css('left','none');
+	    		}//top-right
+	    		else{
+	    			hCard.css({'top':'-20px','height': '40px','display':'block','left':'none'}).find('.hoverCard').css({'bottom':'30px','top':'none','display':'block'});
+		    		hCard.find('.arrow-down').show().css('left','32px');
+		    		hCard.find('.arrow-up').hide().css('left','32px');
+	    		}
+	    	}
+	    	else{
+	    		if(page_width - left < 335){//bottom-left
+		    		hCard.css({'top':'3px','height': '120px','display':'block','left':'-209px'}).find('.hoverCard').css({'top':'24px','bottom':'none','display':'block'});
+		    		hCard.find('.arrow-up').show().css('left','240px');
+		    		hCard.find('.arrow-down').hide().css('left','240px');
+	    		}
+	    		else{//bottom-right
+	    			hCard.css({'top':'3px','height': '120px','display':'block','left':'none'}).find('.hoverCard').css({'top':'24px','bottom':'none','display':'block'});
+		    		hCard.find('.arrow-up').show().css('left','32px');
+		    		hCard.find('.arrow-down').hide().css('left','32px');
+	    		}
+	    	}
+	    }
+        return this;
+ 
+    };
+ 
+	$.fn.hoverCards.defaults = {
+		"url":null,
+		"fadeIn": 800,
+		"fadeOut":400,
+		"backgroundColor":'#ddd',
+		"delay": 1300
+	};
+
+}( jQuery ));
