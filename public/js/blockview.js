c446281669ad0edacb795f339f355a880b284c08
Saideep Bhosle <saideep@outlook.com>
2017-02-20 00:29:51 +0530

    Blockview using CSS tooltips

--- public/js/blockview.js
+++ public/js/blockview.js
@@ -1,41 +1,36 @@
 $(document).ready(function() {
-  var blocks = document.getElementById("hidden-data");
-  var obj = JSON.parse(blocks.innerHTML);
-
-  var i = 0,
-    howManyTimes = 10;
-  var q = document.createElement("div");
-  q.className = "row";
-
-  function loadCards() {
-    var w = document.createElement("div");
-    w.className = "col s6 m2 l3";
-    q.appendChild(w);
-    var e = document.createElement("div");
-    e.className = "card blue-grey darken-1";
-    w.appendChild(e);
-    var r = document.createElement("div");
-    r.className = "card-content white-text";
-    e.appendChild(r);
-    var para = document.createElement("code");
-    para.className = "show_details";
-    var content = document.createTextNode("Block" + (i + 1));
-    r.appendChild(para);
-    para.appendChild(content);
-    document.getElementById("mypara").appendChild(q);
-    var hoverDetails = document.createElement("code");
-    r.appendChild(hoverDetails);
-    hoverDetails.id = i;
-    hoverDetails.className = "hidden-details";
-    var contentx = document.createTextNode(JSON.stringify(obj[i], null, "\t"));
-    console.log(JSON.stringify(obj[i], null, "\t"));
-    hoverDetails.appendChild(contentx);
-
-    i++;
-    if (i < howManyTimes) {
-      setTimeout(loadCards, 100);
+	var blocks = JSON.parse($("#hidden-data").text());
+	var row = document.createElement("div");
+    row.className = "row";                                      // Chain
+    var i;
+    for(i = 0; i < blocks.length; i++) {
+        var block = document.createElement("div");              // Block
+        block.className = "col-md-1 ttip";
+        var x = i + 1;
+        var blocktext = document.createTextNode("B"+ x);
+        block.appendChild(blocktext);
+        var code = document.createElement("div");               // BlockDetails
+        code.className = "ttiptext";
+        if(blocks[i].transactions) {                            // transactions not null
+            var d = new Date(blocks[i].transactions[0].timestamp.seconds * 1000);   // Convert Unix timestamp to JS Date
+            var transdate = code.appendChild(document.createElement("p"));
+            var transtype = code.appendChild(document.createElement("p"));
+            var ccid = code.appendChild(document.createElement("p"));
+            var payload = code.appendChild(document.createElement("p"));
+            transdate.appendChild(document.createTextNode("Created:\t" + d.toString() +""));
+            transtype.appendChild(document.createTextNode("Type:\t" + blocks[i].transactions[0].type));
+            ccid.appendChild(document.createTextNode("CCID:\t" + blocks[i].transactions[0].chaincodeID));
+            payload.appendChild(document.createTextNode("Payload:\t" + blocks[i].transactions[0].payload));
+        } else {
+            var d = new Date(blocks[i].nonHashData.localLedgerCommitTimestamp.seconds * 1000);      // Date in non transactional block
+            var transdate = code.appendChild(document.createElement("p"));
+            transdate.appendChild(document.createTextNode("Created:\t" + d.toString() +""));
+        }
+        block.appendChild(code);
+        row.appendChild(block);
     }
-  }
-  loadCards();
-
+    document.getElementById("blockview").appendChild(row);
+    $(".ttip").click(function() {
+        $(this).find(".ttiptext").toggleClass("show");
+    });
 });
